c87d9129c4cc6bdc3cd7f9e5991c3b7b8033c8ad
Alnur Kabdolla <62064152+akab1902@users.noreply.github.com>
2023-03-08 16:09:29 +0800

    add record video

--- lib/screens/camera/page/camera_page.dart
+++ lib/screens/camera/page/camera_page.dart
@@ -3,12 +3,11 @@ import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:personal_training_app/screens/camera/bloc/camera_bloc.dart';
-import 'package:personal_training_app/screens/camera/bloc/camera_utils.dart';
 import 'package:personal_training_app/screens/camera/widget/record_button_widget.dart';
 
+import '../../../core/ common_widgets/loading_widget.dart';
 import '../../../core/const/color_constants.dart';
+import '../../../core/service/logger.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({Key? key}) : super(key: key);
@@ -18,38 +17,28 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
-  Duration duration = Duration();
-  Timer? timer;
-  String val = 'Squats';
-  bool isRecording = false;
+  Duration _duration = Duration();
+  Timer? _timer;
+  String _val = 'Squats';
+  bool _isRecording = false;
+  bool _isLoading = true;
+  late CameraController _cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildBody(context));
-  }
-
-  BlocProvider<CameraBloc> _buildBody(BuildContext context) {
-    return BlocProvider<CameraBloc>(
-      create: (context) => CameraBloc(cameraUtils: CameraUtils()),
-      child: BlocConsumer<CameraBloc, CameraState>(
-        buildWhen: (_, currState) => currState is CameraInitial,
-        builder: (context, state) {
-          final bloc = BlocProvider.of<CameraBloc>(context);
-          if (state is CameraInitial) {
-            bloc.add(CameraInitialized());
-          }
-          return cameraContent();
-        },
-        listenWhen: (_, currState) => currState is ErrorState,
-        listener: (context, state) {
-          if (state is ErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(state.message)),
-            );
-          }
-        },
-      ),
-    );
+    return cameraContent();
   }
 
   Widget cameraContent() {
@@ -58,19 +47,11 @@ class _CameraPageState extends State<CameraPage> {
       color: ColorConstants.backgroundWhite,
       child: Stack(
         children: [
-          BlocBuilder<CameraBloc, CameraState>(
-            buildWhen: (_, currState) => currState is CameraReadyState,
-            builder: (context, state) {
-              return state is CameraReadyState &&
-                      BlocProvider.of<CameraBloc>(context).getController() !=
-                          null
-                  ? SizedBox(
-                      height: double.infinity,
-                      child: CameraPreview(BlocProvider.of<CameraBloc>(context)
-                          .getController()!))
-                  : Container();
-            },
-          ),
+          !_isLoading
+              ? SizedBox(
+              height: double.infinity,
+              child: CameraPreview(_cameraController))
+              : _createLoading(),
           Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -105,77 +86,65 @@ class _CameraPageState extends State<CameraPage> {
 
   Widget _createDropDownMenu() {
     final items = ['Squats', 'Push-ups', 'Pull-ups'];
-    return BlocBuilder<CameraBloc, CameraState>(
-      buildWhen: (_, currState) => currState is CameraReadyState,
-      builder: (context, state) {
-        return DropdownButtonHideUnderline(
-          child: DropdownButton2(
-            items: items
-                .map((item) => DropdownMenuItem<String>(
-                      value: item,
-                      child: Text(
-                        item,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ))
-                .toList(),
-            value: val,
-            dropdownStyleData: DropdownStyleData(
-              width: 130,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(14),
-                color: ColorConstants.primaryColor,
-              ),
-            ),
-            buttonStyleData: const ButtonStyleData(width: 130),
-            iconStyleData: const IconStyleData(
-              icon: Icon(Icons.keyboard_arrow_up, color: ColorConstants.white),
-            ),
-            onChanged: (value) {
-              setState(() {
-                val = value!;
-              });
-            },
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2(
+        items: items
+            .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(
+                    item,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ))
+            .toList(),
+        value: _val,
+        dropdownStyleData: DropdownStyleData(
+          width: 130,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            color: ColorConstants.primaryColor,
           ),
-        );
-      },
+        ),
+        buttonStyleData: const ButtonStyleData(width: 130),
+        iconStyleData: const IconStyleData(
+          icon: Icon(Icons.keyboard_arrow_up, color: ColorConstants.white),
+        ),
+        onChanged: (value) {
+          setState(() {
+            _val = value!;
+          });
+        },
+      ),
     );
   }
 
   Widget _createButton() {
-    return BlocBuilder<CameraBloc, CameraState>(
-      buildWhen: (_, currState) =>
-          currState is CameraReadyState ||
-          currState is CameraRecordingInProgressState ||
-          currState is CameraRecordingSuccessState ||
-          currState is CameraRecordingErrorState,
-      builder: (context, state) {
-        final bloc = BlocProvider.of<CameraBloc>(context);
-        return BlocProvider.of<CameraBloc>(context).getController() != null
-            ? RecordButton(
-            isRecording: isRecording,
-            onStart: () {
-              isRecording = true;
-                startTimer();
-                // bloc.add(CameraStartRecording());
-              }, onStop: () {
-          isRecording = false;
-          stopTimer();
-                // bloc.add(CameraStopRecording());
-              })
-            : Container();
-      },
-    );
+    return RecordButton(
+                isRecording: _isRecording,
+                onStart: () {
+                  _recordVideo();
+                  startTimer();
+                  // bloc.add(CameraStartRecording());
+                },
+                onStop: () {
+                  _recordVideo();
+                  stopTimer();
+                  // bloc.add(CameraStopRecording());
+                });
+  }
+
+  Widget _createLoading(){
+    return const LoadingWidget();
   }
 
   Widget _createTimer() {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    final minutes = twoDigits(_duration.inMinutes.remainder(60));
+    final seconds = twoDigits(_duration.inSeconds.remainder(60));
 
     return Text(
       "$minutes:$seconds",
@@ -187,20 +156,41 @@ class _CameraPageState extends State<CameraPage> {
   }
 
   void startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
   }
 
   void stopTimer() {
     setState(() {
-      duration = const Duration();
-      timer?.cancel();
+      _duration = const Duration();
+      _timer?.cancel();
     });
   }
 
   void addTime() {
     setState(() {
-      final seconds = duration.inSeconds + 1;
-      duration = Duration(seconds: seconds);
+      final seconds = _duration.inSeconds + 1;
+      _duration = Duration(seconds: seconds);
     });
+  }
+
+  _initCamera() async {
+    final cameras = await availableCameras();
+    final front = cameras.firstWhere(
+        (camera) => camera.lensDirection == CameraLensDirection.front);
+    _cameraController = CameraController(front, ResolutionPreset.max);
+    await _cameraController.initialize();
+    setState(() => _isLoading = false);
+  }
+
+  _recordVideo() async {
+    if (_isRecording) {
+      final file = await _cameraController.stopVideoRecording();
+      setState(() => _isRecording = false);
+      logger.d(file.name);
+    } else {
+      await _cameraController.prepareForVideoRecording();
+      await _cameraController.startVideoRecording();
+      setState(() => _isRecording = true);
+    }
   }
 }
